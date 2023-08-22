fa693e51e688d77cf5c2b0e5fa1d124b19d7dd1c
Hamid Reza Hossini <power22xx@outlook.com>
2023-08-22 12:37:45 +0330

    add js file menu.js

--- src/js/menu-js.js
+++ src/js/menu-js.js
@@ -3,21 +3,28 @@ var closeicon=document.querySelector(".closeicon");
 var menumobile=document.querySelector(".menumobile");
 var submenuitem=document.querySelectorAll(".submenuitem");
 var submenuitem1=document.querySelectorAll(".submenuitem ul");
+var arrowmenumobile=document.querySelectorAll(".arrowmenumobile");
 
 menuicon.addEventListener("click",()=>{
     menuicon.classList.toggle("hidden");
     closeicon.classList.toggle("hidden");
-    menumobile.classList.remove("hidden")
+    menumobile.classList.remove("h-[0px]")
+    menumobile.classList.add("h-[750px]")
+
 });
 
 closeicon.addEventListener("click",()=>{
     menuicon.classList.toggle("hidden");
     closeicon.classList.toggle("hidden");
-    menumobile.classList.add("hidden");
+    menumobile.classList.remove("h-[750px]")
+    menumobile.classList.add("h-[0px]")
+
 });
 
 submenuitem.forEach((item,index)=>{
     item.addEventListener("click",()=>{
-        submenuitem1[index].classList.toggle("hidden")
+        submenuitem1[index].classList.toggle("h-[0px]")
+        submenuitem1[index].classList.toggle("h-[200px]")
+        arrowmenumobile[index].classList.toggle("rotate-180");
     });
 });
